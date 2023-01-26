68a504242a75e73e8388816ed7c2a4509417fcb9
Debapriya Dhar <debapriyadhar@Debapriyas-MacBook-Air.local>
2023-01-27 04:48:02 +0600

    added video and audio

---
+++ componants/VideoPlayer.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const VideoPlayer = ({url}) => {
+
+  return (
+    <>
+        <video  id="movie" src={url} height="900px" width="1300px" controls autoplay > </video>';
+    </>  
+  );
+};
+// test-3ce96
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 export default function Home() {
   
   let name = "Guest";
-  useEffect(()=>{
-    let user = JSON.parse(localStorage.getItem("user"));
-    if(user.isLoggedIn){
-      name = user.role
-    }
+  // useEffect(()=>{
+  //   let user = JSON.parse(localStorage.getItem("user"));
+  //   if(user.isLoggedIn){
+  //     name = user.role
+  //   }
 
-  },[])
+  // },[])
   return (
     <div
       style={{
